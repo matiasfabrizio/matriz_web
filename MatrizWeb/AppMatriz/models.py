1cad29fc983e69324c4d2afb671459f3ae6ca0b3
Matias Prado <prad0031@algonquinlive.com>
2024-02-27 06:26:05 -0500

    Fixed cambios hechos al modelo de la web.

--- MatrizWeb/AppMatriz/models.py
+++ MatrizWeb/AppMatriz/models.py
@@ -3,9 +3,14 @@ from django.db import models
 class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
-    horario = models.CharField(max_length=7)
+    horario = models.CharField(max_length=17)
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Horario: {self.horario}"
 
 class Profesor(models.Model):
 
     nombre = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f"Nombre: {self.nombre}"
