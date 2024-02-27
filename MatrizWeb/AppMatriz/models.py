31b76ede68f1908e8555f4b9c586474817a3bc3a
Matias Prado <prad0031@algonquinlive.com>
2024-02-27 06:11:33 -0500

    Cambios estéticos finalizados, olvidé hacer commit. Actualizados modelos para hacer CRUD y que la web sea editable fácil y rápidamente.

--- MatrizWeb/AppMatriz/models.py
+++ MatrizWeb/AppMatriz/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    horario = models.CharField(max_length=7)
+
+class Profesor(models.Model):
+
+    nombre = models.CharField(max_length=40)
+
