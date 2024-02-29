04e3123e1463be5d7c12bdfbf21f7446a9caa988
Matias Prado <prad0031@algonquinlive.com>
2024-02-29 08:22:07 -0500

    Corrección de modelo para agregarle un nombre a cada horario.

--- MatrizWeb/AppMatriz/models.py
+++ MatrizWeb/AppMatriz/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Horario(models.Model):
 
+    nombre = models.CharField(max_length=40, default='no_name')
     imagen = models.ImageField(upload_to='AppMatriz/assets/horario')
 
     def __str__(self) -> str:
-        return f'Horario'
+        return f'Nombre: {self.nombre}'
